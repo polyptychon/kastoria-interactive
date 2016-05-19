cd02bc560c5bb4d3fd0b116d1f390491bedf6786
Harris Sidiropoulos <harris.sidiropoulos@gmail.com>
2016-05-19 15:51:16 +0300

    set kiosk mode

--- main.js
+++ main.js
@@ -11,12 +11,14 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.setMenuBarVisibility(false)
+  mainWindow.setKiosk(true)
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/builds/development/index.html`)
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
