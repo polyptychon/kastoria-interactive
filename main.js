802656849436b05af4e830b14f55ba1cdf21d4ca
Harris Sidiropoulos <harris.sidiropoulos@gmail.com>
2016-05-20 11:35:36 +0300

    add batch

--- main.js
+++ main.js
@@ -8,6 +8,30 @@ const BrowserWindow = electron.BrowserWindow
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+// var Kinect2 = require('kinect2'),
+//     express = require('express'),
+//     express_app = express(),
+//     server = require('http').createServer(express_app),
+//     io = require('socket.io').listen(server);
+
+// var kinect = new Kinect2();
+
+// if(kinect.open()) {
+//     server.listen(8000);
+//     console.log('Server listening on port 8000');
+//     console.log('Point your browser to http://localhost:8000');
+//     express_app.use(express.static('builds/development'));
+//     express_app.get('/', function(req, res) {
+//         res.sendFile(__dirname + '/builds/development/index.html');
+//     });
+//     kinect.on('bodyFrame', function(bodyFrame){
+//         io.sockets.emit('bodyFrame', bodyFrame);
+//     });
+
+//     kinect.openBodyReader();
+// }
+
+
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
