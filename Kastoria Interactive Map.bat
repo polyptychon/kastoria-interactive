7b48fd96ed44bb9e749737f939a5188b34789e98
KastoriaByzantineMuseum <kastoria.byzantine.museum@polyptychon.gr>
2016-05-20 14:30:31 +0300

    clean up

--- Kastoria Interactive Map.bat
+++ Kastoria Interactive Map.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-start cmd.exe /K "cd C:\Users\ser\Documents\GitHub\kastoria-interactive && node broadcast.js"
-start cmd.exe /K "cd C:\Users\ser\Documents\GitHub\kastoria-interactive && npm start"
+start cmd.exe /K "cd C:\Users\user\Documents\GitHub\kastoria-interactive && node broadcast.js"
+start cmd.exe /K "cd C:\Users\user\Documents\GitHub\kastoria-interactive && npm start"
